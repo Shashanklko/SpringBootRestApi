2049aba6d7d06661e5f1bc63cd64a5433d5755f8
Shashank <Shashankkm2003@gmail.com>
2025-12-10 01:49:17 +0530

    Just learn SpringBoot RESTapi and perform crud operation

---
+++ src/main/java/com/example/SpringBootRestApi/entity/Product.java
@@ -0,0 +1,53 @@
+package com.example.SpringBootRestApi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Product")
+public class Product{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
